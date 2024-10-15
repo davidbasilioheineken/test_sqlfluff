a48e44f97a253e2ae16923941ee84ffdb1e97e82
davidbasilioheineken <davidbasilioheineken>
2024-10-15 21:13:54 +0000

    new table1

--- sample_merge.sql
+++ sample_merge.sql
@@ -2,7 +2,7 @@
 -- Create a temporary view with distinct rows and enriched data from user_info
 CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW  AS
 select *
-from (select distinct * from myTable)
+from (select distinct * from table1)
 join user_info using(user_id);
 
 -- Perform the MERGE operation
