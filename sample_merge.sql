0e677b3edccdc033a5662097763f92f1c82439f8
davidbasilioheineken <davidbasilioheineken>
2024-10-15 21:05:53 +0000

    extra space

--- sample_merge.sql
+++ sample_merge.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- Create a temporary view with distinct rows and enriched data from user_info
-CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW AS
+CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW  AS
 select *
 from (select distinct * from myTable)
 join user_info using(user_id);
