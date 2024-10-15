4bc0d5c00b4de5af74fe24306426b994d9d4956f
davidbasilioheineken <davidbasilioheineken>
2024-10-15 21:52:04 +0000

    lower logs

--- sample_merge.sql
+++ sample_merge.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- Create a temporary view with distinct rows and enriched data from user_info
-CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW  AS
+CREATE TEMPORARY VIEW new_logs  AS
 SELECT *
 FROM (SELECT DISTINCT * FROM tbl)
 JOIN user_info USING(user_id);
