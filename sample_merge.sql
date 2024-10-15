0f0d825f1268895c4109d0efa2ad79d0a55659bf
davidbasilioheineken <davidbasilio.rodriguezcortez@heineken.com>
2024-10-15 22:27:35 +0000

    Auto-fix SQL file with SQLFluff

--- sample_merge.sql
+++ sample_merge.sql
@@ -1,4 +1,4 @@
 -- Databricks notebook source
-CREATE TEMPORARY VIEW new_logs  AS
-select column1
-FROM (SELECT DISTINCT * FROM TBL);
+CREATE TEMPORARY VIEW new_logs AS
+SELECT column1
+FROM (SELECT DISTINCT * FROM tbl);
