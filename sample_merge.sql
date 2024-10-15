cf861c893ed3110a84d4f66d5891cc0bd68104ca
davidbasilioheineken <davidbasilioheineken>
2024-10-15 22:54:46 +0000

    many mistakes

--- sample_merge.sql
+++ sample_merge.sql
@@ -1,16 +1,16 @@
 -- Databricks notebook source
 CREATE TEMPORARY VIEW new_logs AS
-SELECT
-    column1,
-    col2,
+select
+    column1
+    ,col2,
     col3
-FROM (SELECT DISTINCT * FROM tbl);
+from (select distinct * from logs);
 
 -- COMMAND ----------
 
 -- Create a temporary view with distinct rows and enriched data from user_info
 CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW AS
-select col1
+select *
 from (select distinct * from dedupedLogs)
 join user_info using(user_id);
 
