0cbdce7358d4015ec52c66578a84cb87230fa5be
davidbasilioheineken <davidbasilioheineken>
2024-10-15 20:46:35 +0000

    new sql file

---
+++ sample_merge.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+-- Create a temporary view with distinct rows and enriched data from user_info
+CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW AS
+select *
+from (select distinct * from dedupedLogs)
+join user_info using(user_id);
+
+-- Perform the MERGE operation
+merge into logs using new_deduped_logs_view on logs.uniqueid = new_deduped_logs_view.uniqueid when not matched then insert *;
