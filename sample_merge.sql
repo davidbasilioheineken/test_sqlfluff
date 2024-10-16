93dae4e08cd1d00ca8186c42e832a4af581957f7
davidbasilioheineken <davidbasilioheineken>
2024-10-16 22:49:57 +0000

    remove many mistakes

--- sample_merge.sql
+++ sample_merge.sql
@@ -5,14 +5,3 @@ select
     ,col2,
     col3
 from (select distinct * from logs);
-
--- COMMAND ----------
-
--- Create a temporary view with distinct rows and enriched data from user_info
-CREATE TEMPORARY VIEW NEW_DEDUPED_LOGS_VIEW AS
-select *
-from (select distinct * from dedupedLogs)
-join user_info using(user_id);
-
--- Perform the MERGE operation
-merge into logs using new_deduped_logs_view on logs.uniqueid = new_deduped_logs_view.uniqueid when not matched then insert *;
