56648f83038e3d15b9468118e97b2d6057124b5f
davidbasilioheineken <davidbasilio.rodriguezcortez@heineken.com>
2024-10-16 23:00:35 +0000

    Auto-fix SQL file with SQLFluff

--- code/dlt.sql
+++ code/dlt.sql
@@ -1,26 +1,26 @@
 -- Databricks notebook source
-CREATE OR REFRESH STREAMING TABLE tbl_stg_mae 
+CREATE OR REFRESH STREAMING TABLE tbl_stg_mae
 (
-  CONSTRAINT valid_records 
+    CONSTRAINT valid_records
     EXPECT (
-            NUM_CLIENTE IS NOT NULL and 
-            NOMBRE_CLIENTE IS NOT NULL
-            ) 
+        num_cliente IS NOT NULL
+        AND nombre_cliente IS NOT NULL
+    )
     ON VIOLATION DROP ROW
 )
 COMMENT "This table contains all valid records for MAE"
-TBLPROPERTIES 
-(
-  "quality" = "silver", 
-  "delta.enableChangeDataFeed" = "true", 
-  "department" = "Sales Growth Planning", 
-  "source" = "SAP"
-);
+    TBLPROPERTIES
+    (
+        "quality" = "silver",
+        "delta.enableChangeDataFeed" = "true",
+        "department" = "Sales Growth Planning",
+        "source" = "SAP"
+    );
 
 -- COMMAND ----------
 
-APPLY CHANGES INTO LIVE.tbl_stg_mae 
+APPLY CHANGES INTO live.tbl_stg_mae
 FROM STREAM(heiaepmx001dwe01.heiaepmxddb_ing.tbl_ing_mae)
-  KEYS (NUM_CLIENTE, ORG_VENTAS)
-  SEQUENCE BY processed_date
-     STORED AS SCD TYPE 2;
+KEYS (num_cliente, org_ventas)
+SEQUENCE BY processed_date
+STORED AS SCD TYPE 2;
--- code/sample_merge.sql
+++ code/sample_merge.sql
@@ -1,7 +1,7 @@
 -- Databricks notebook source
 CREATE TEMPORARY VIEW new_logs AS
-select
-    column1
-    ,col2,
+SELECT
+    column1,
+    col2,
     col3
-from (select distinct * from logs);
+FROM (SELECT DISTINCT * FROM logs);
