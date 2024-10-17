e4e21bd6dd7eedde8fb2812346a22cdb9e9b9f2f
davidbasilioheineken <davidbasilioheineken>
2024-10-17 17:40:29 +0000

    few mistakes

--- code/sample_merge.sql
+++ code/sample_merge.sql
@@ -1,7 +1,7 @@
 -- Databricks notebook source
 CREATE TEMPORARY VIEW new_logs AS
 SELECT
-    column1,
-    col2,
+    column1,    col2,
     col3
-FROM (SELECT DISTINCT * FROM logs);
+    ,COL4
+from(SELECT DISTINCT * FROM logs);
