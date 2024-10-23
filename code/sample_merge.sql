e9ad5374fcffd0e3593586ba6b04e5d491ac8bd2
davidbasilioheineken <davidbasilioheineken>
2024-10-23 21:09:02 +0000

    check if files are modified

--- code/sample_merge.sql
+++ code/sample_merge.sql
@@ -1,8 +1,8 @@
 -- Databricks notebook source
 CREATE TEMPORARY VIEW NEW_LOGS AS
 SELECT
-    COLUMN1 AS COL1,
-    COL2,
+    COLUMN1 as COL1,
+    col2,
     COL3,
     COL4
 FROM (SELECT DISTINCT * FROM LOGS);
