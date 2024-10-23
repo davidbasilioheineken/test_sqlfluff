dbf66e2f74475cf1a5c3e0ab980d2ccef8877604
davidbasilioheineken <177360613+davidbasilioheineken@users.noreply.github.com>
2024-10-23 22:38:26 +0000

    Auto-fix SQL file(s) with SQLFluff

--- code/dlt.sql
+++ code/dlt.sql
@@ -1,15 +1,15 @@
 -- Databricks notebook source
-CREATE or REFRESH STREAMING TABLE tbl_stg_mae
+CREATE OR REFRESH STREAMING TABLE tbl_stg_mae
 (
     CONSTRAINT valid_records
     EXPECT (
         num_cliente IS NOT NULL
-        and nombre_cliente IS NOT NULL
+        AND nombre_cliente IS NOT NULL
     )
-    on VIOLATION DROP ROW
+    ON VIOLATION DROP ROW
 )
 COMMENT "This table contains all valid records for MAE"
-    
+
     TBLPROPERTIES
     (
         "quality" = "silver",
@@ -22,7 +22,9 @@ COMMENT "This table contains all valid records for MAE"
 
 APPLY CHANGES INTO live.tbl_stg_mae
 FROM STREAM(heiaepmx001dwe01.heiaepmxddb_ing.tbl_ing_mae)
-KEYS(num_cliente,
-org_ventas)
-SEQUENCE  by processed_date
-STORED AS SCD TYPE   2;
+KEYS (
+    num_cliente,
+    org_ventas
+)
+SEQUENCE BY processed_date
+STORED AS SCD TYPE 2;
