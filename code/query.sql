28bc5ea5bc2b8309ca236ea9c0b7250b2f3b17a2
davidbasilioheineken <177360613+davidbasilioheineken@users.noreply.github.com>
2024-10-24 19:33:49 +0000

    Auto-fix SQL file(s) with SQLFluff

--- code/query.sql
+++ code/query.sql
@@ -1,16 +1,23 @@
 -- Databricks notebook source
-CREATE  SCHEMA IF NOT EXISTS `main`.`billing_forecast`;
+CREATE SCHEMA IF NOT EXISTS `main`.`billing_forecast`;
 CREATE TABLE IF NOT EXISTS `main`.`billing_forecast`.billing_forecast (sku STRING, workspace_id STRING);
-INSERT INTO `main`.`billing_forecast`.billing_forecast (sku, workspace_id) SELECT 'ALL', 'ALL'
-  WHERE NOT EXISTS (SELECT * FROM  `main`.`billing_forecast`.billing_forecast);
+INSERT INTO `main`.`billing_forecast`.billing_forecast (sku, workspace_id) SELECT
+    'ALL',
+    'ALL'
+WHERE NOT EXISTS (SELECT * FROM `main`.`billing_forecast`.billing_forecast);
 
 -- COMMAND ----------
 
-select distinct(workspace_id) from `main`.`billing_forecast`.billing_forecast 
-  where now() > '2000-01-01' -- temporary workaround to remove cache due to the way dbdemos import query - will be removed in lakeview
-order by workspace_id DESC;
+SELECT DISTINCT workspace_id FROM `main`.`billing_forecast`.billing_forecast
+-- temporary workaround to remove cache due to the way dbdemos import query - will be removed in lakeview
+WHERE NOW() > '2000-01-01'
+ORDER BY workspace_id DESC;
 
 -- COMMAND ----------
 
-SELECT gender, count(gender) as total_churn FROM `main`.`dbdemos_retail_c360`.churn_features where churn = 1 GROUP BY gender
-
+SELECT
+    gender,
+    COUNT(gender) AS total_churn
+FROM `main`.`dbdemos_retail_c360`.churn_features
+WHERE churn = 1
+GROUP BY gender
--- code/query_2.sql
+++ code/query_2.sql
@@ -1,35 +1,49 @@
 -- Databricks notebook source
-SELECT p.country, p.churn, count(*) as customers
-	FROM `main`.`dbdemos_retail_c360`.churn_features p
-	GROUP BY p.country, p.churn
+SELECT
+    p.country,
+    p.churn,
+    COUNT(*) AS customers
+FROM `main`.`dbdemos_retail_c360`.churn_features AS p
+GROUP BY p.country, p.churn
 
 -- COMMAND ----------
 
-SELECT canal,sum(amount)/100 as MRR FROM `main`.`dbdemos_retail_c360`.churn_orders o 
-		INNER JOIN `main`.`dbdemos_retail_c360`.churn_users using (user_id)
-    group by canal;
+SELECT
+    canal,
+    SUM(amount) / 100 AS mrr
+FROM `main`.`dbdemos_retail_c360`.churn_orders
+INNER JOIN `main`.`dbdemos_retail_c360`.churn_users ON churn_orders.user_id = churn_users.user_id
+GROUP BY canal;
 
 -- COMMAND ----------
 
-with customer_total_return as
-(select sr_customer_sk as ctr_customer_sk
-,sr_store_sk as ctr_store_sk
-,sum(SR_FEE) as ctr_total_return
-from store_returns
-,date_dim
-where sr_returned_date_sk = d_date_sk
-and d_year =2000
-group by sr_customer_sk
-,sr_store_sk)
- select  *
-from customer_total_return ctr1
-,store
-,customer
-where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
-from customer_total_return ctr2
-where ctr1.ctr_store_sk = ctr2.ctr_store_sk)
-and s_store_sk = ctr1.ctr_store_sk
-and s_state = 'TN'
-and ctr1.ctr_customer_sk = c_customer_sk
-order by c_customer_id
+WITH customer_total_return AS (
+    SELECT
+        sr_customer_sk AS ctr_customer_sk,
+        sr_store_sk AS ctr_store_sk,
+        SUM(sr_fee) AS ctr_total_return
+    FROM store_returns,
+        date_dim
+    WHERE
+        sr_returned_date_sk = d_date_sk
+        AND d_year = 2000
+    GROUP BY
+        sr_customer_sk,
+        sr_store_sk
+)
+
+SELECT *
+FROM customer_total_return AS ctr1,
+    store,
+    customer
+WHERE
+    ctr1.ctr_total_return > (
+        SELECT AVG(ctr_total_return) * 1.2
+        FROM customer_total_return AS ctr2
+        WHERE ctr1.ctr_store_sk = ctr2.ctr_store_sk
+    )
+    AND s_store_sk = ctr1.ctr_store_sk
+    AND s_state = 'TN'
+    AND ctr1.ctr_customer_sk = c_customer_sk
+ORDER BY c_customer_id
 LIMIT 100;
