9af9d332e886bb0e69beaaffaebad3c1bd6fd76e
davidbasilioheineken <177360613+davidbasilioheineken@users.noreply.github.com>
2024-10-25 16:12:34 +0000

    Auto-fix SQL file(s) with SQLFluff

--- code/sample_merge.sql
+++ code/sample_merge.sql
@@ -1,6 +1,8 @@
 -- Databricks notebook source
 CREATE TEMPORARY VIEW NEW_LOGS AS
-select
-    col COL1,
-    COL2,    COL3,COL4
-FROM(SELECT DISTINCT * FROM LOGS);
+SELECT
+    COL AS COL1,
+    COL2,
+    COL3,
+    COL4
+FROM (SELECT DISTINCT * FROM LOGS);
